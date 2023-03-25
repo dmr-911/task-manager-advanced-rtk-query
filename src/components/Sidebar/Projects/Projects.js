a756b0d14680a17c77f82db43de63a413ced796f
dmr-911 <dewan.mizanur911@gmail.com>
2023-03-25 15:39:55 +0600

    projects api implemented

--- src/components/Sidebar/Projects/Projects.js
+++ src/components/Sidebar/Projects/Projects.js
@@ -1,35 +1,34 @@
 import React from "react";
+import { useGetProjectsQuery } from "../../../features/projects/projectsApi";
 
 const Projects = () => {
+  const { data: projects, isLoading, isError, error } = useGetProjectsQuery();
+
+  // decide what to render
+  let content = null;
+
+  if (isLoading) content = <div>Loading team projects...</div>;
+
+  if (!isLoading && isError) content = <div>{error}</div>;
+
+  if (!isLoading && !isError && !projects?.length)
+    content = <div>No projects found!</div>;
+
+  if (!isLoading && !isError && projects?.length)
+    content = projects.map((project) => {
+      const { id, projectName, colorClass } = project || {};
+      return (
+        <div className="checkbox-container" key={id}>
+          <input type="checkbox" className={colorClass} defaultChecked />
+          <p className="label">{projectName}</p>
+        </div>
+      );
+    });
+
   return (
     <div>
       <h3 className="text-xl font-bold">Projects</h3>
-      <div className="mt-3 space-y-4">
-        <div className="checkbox-container">
-          <input type="checkbox" className="color-scoreboard" defaultChecked />
-          <p className="label">Scoreboard</p>
-        </div>
-        <div className="checkbox-container">
-          <input type="checkbox" className="color-flight" defaultChecked />
-          <p className="label">Flight Booking</p>
-        </div>
-        <div className="checkbox-container">
-          <input type="checkbox" className="color-productCart" defaultChecked />
-          <p className="label">Product Cart</p>
-        </div>
-        <div className="checkbox-container">
-          <input type="checkbox" className="color-bookstore" defaultChecked />
-          <p className="label">Book Store</p>
-        </div>
-        <div className="checkbox-container">
-          <input type="checkbox" className="color-blog" defaultChecked />
-          <p className="label">Blog Application</p>
-        </div>
-        <div className="checkbox-container">
-          <input type="checkbox" className="color-jobFinder" defaultChecked />
-          <p className="label">Job Finder</p>
-        </div>
-      </div>
+      <div className="mt-3 space-y-4">{content}</div>
     </div>
   );
 };
